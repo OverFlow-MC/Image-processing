b32f7a9ef14eae00b1016e9baac11c3eafb669f9
Julien <julienvouilloz2000@gmail.com>
2025-09-09 13:55:57 +0200

    Update More contrast filter (you can use your own filter).py

--- More contrast filter (you can use your own filter).py
+++ More contrast filter (you can use your own filter).py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-os.chdir("D:/Informatique/TP11")
-T=plt.imread("Couchersoleil.jpg")
+os.chdir("D:/...")
+T=plt.imread("YourImage.jpg")
 n,m,p=np.shape(T)
 T2=np.zeros([n,m,p],dtype='uint8')
 F=np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
@@ -17,4 +17,5 @@ for i in range(1,n-1):
                 niv=0
             T2[i,j,k]=niv
 plt.imshow(T2)
+
 plt.show()
