57590290697a686fcd43441fb9fbc0ef3c509fcd
Julien <julienvouilloz2000@gmail.com>
2025-09-09 13:53:31 +0200

    Update 90 degrees image rotation.py

--- 90 degrees image rotation.py
+++ 90 degrees image rotation.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 def rotation(x):
-    os.chdir("D:/Informatique/TP11")
-    T=plt.imread("Couchersoleil.jpg")
+    os.chdir("D:/...")
+    T=plt.imread("YourImage.jpg")
     m,n,p=np.shape(T)
     T2=np.zeros([m,n,3],dtype='uint8')
     for i in range(n):
         T2[:,n-i-1,:]=T[i,:,:]
     plt.imshow(T2)
     plt.show()
+
