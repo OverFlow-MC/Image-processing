7ea78a0f1cfaf929d02f0fb01d37ed13f47f9084
Julien <julienvouilloz2000@gmail.com>
2025-09-09 13:55:28 +0200

    Update Is an image in color ?.py

--- Is an image in color ?.py
+++ Is an image in color ?.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 def test_image(x):
-    os.chdir("D:/Informatique/TP11")
-    a=plt.imread('PaysageNB.jpg')
+    os.chdir("D:/...")
+    a=plt.imread('YourImage.jpg')
     b,c,d=np.shape(a)
     for i in range(b):
         for j in range(c):
             if not(a[i,j,0]==a[i,j,1]==a[i,j,2]):
-                return "l'image est en couleur"
-    return "l'image est en noir et blanc"
+                return "The image is in color"
+
+    return "The image is in black and white"
